d7facdfdf504fce9aecfa7fccbe23a01f4856da2
vallabhgagliya <vallabhgagliya10@gmail.com>
2024-06-21 19:40:46 +0530

    task 2

--- simple_calculator.cpp
+++ simple_calculator.cpp
@@ -1,1 +1,52 @@
-// Calculator
+#include<iostream>
+
+
+using namespace std;
+
+int main(){
+    int num1,num2;
+    char operation;
+    double result;
+    
+    cout<<"enter 1st number:";
+    cin>>num1;
+
+    cout<<"enter a operation of this ( + , - , * , / ) :";
+    cin>>operation;
+
+    cout<<"enter 2st number:";
+    cin>>num2;
+
+    switch (operation){
+        case '+' :
+
+        result = num1 + num2;
+        cout<< num1<<" + "<<num2<<" = " <<result<<endl;
+        break;
+
+        case '-' :
+
+        result = num1 - num2;
+        cout<< num1<<" - "<<num2<<" = "  <<result<<endl;
+        break;
+
+        case '*' :
+
+        result = num1 * num2;
+        cout<< num1<<" * "<<num2<<" = "  <<result<<endl;
+        break;
+
+        case '/' :
+
+        result = num1 / num2;
+        cout<< num1<<" / "<<num2<<" = "  <<result<<endl;
+        break;
+
+    default:
+            cout << "Error: Invalid operator!" << endl;
+            break;
+    }
+    return 0;
+}
+
+
